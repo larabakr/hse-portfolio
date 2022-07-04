443dbc0fb656936bee2137b910184e1493f385a8
uwuxia <uwuxia@uwuxias-MacBook-Air.local>
2022-07-04 19:42:03 +0300

    prototype change 2

--- comps/demos.tsx
+++ comps/demos.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/demos.module.scss';
+import styles from "../styles/demos.module.scss";
 
 const Demos = () => {
   return (
@@ -8,8 +8,8 @@ const Demos = () => {
         <div className={styles.left}>
           <div className={styles.cont}>
             <iframe
-               width="100%"
-               height={315 * 1.2}
+              width="560"
+              height="315"
               src="https://www.youtube.com/embed/OryIHb72xFM"
               title="YouTube video player"
               frameBorder="0"
@@ -25,9 +25,9 @@ const Demos = () => {
         <div className={styles.right}>
           <div className={styles.cont}>
             <iframe
-              width="100%"
-              height={315 * 1.2}
-              src="https://www.youtube.com/embed/uZdubwSyU5Q"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/khozk8HJ3TQ"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
