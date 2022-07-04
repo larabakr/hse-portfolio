78bd57ee668dd14f0912ee1f6371571b4861bf98
uwuxia <uwuxia@uwuxias-MacBook-Air.local>
2022-07-04 18:52:16 +0300

    prototype change 1

--- comps/navbar.tsx
+++ comps/navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import logo from './logo.webp';
 
 const Navbar = () => {
   return (
@@ -6,7 +7,7 @@ const Navbar = () => {
       <nav>
         <div className="logo">
           <Image
-            src="/favicon.png"
+            src={logo}
             draggable="false"
             height="75px"
             width="75px"
